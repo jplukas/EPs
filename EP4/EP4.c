e9f3cdb02e69043cee24d279402beb7266ae920c
jplukas <jplukasavicus@gmail.com>
2018-11-27 06:14:50 -0200

    hum

--- EP4/EP4.c
+++ EP4/EP4.c
@@ -15,6 +15,12 @@ typedef struct elo_ll{
     tipo_palavra* palavra;
 }elo_ll;
 
+typedef struct arvore{
+    tipo_palavra* palavra;
+    struct arvore* esq;
+    struct arvore* dir;
+}arvore;
+
 elo_ll* criaElo(char* palavra, int ocorrencias){
     elo_ll* elo = malloc(sizeof(elo_ll));
     int tam = strlen(palavra);
@@ -79,7 +85,6 @@ elo_ll* buscaOrdenadaLl(elo_ll* lista, char* palavra, elo_ll** pos_insere){
     return NULL;
 }
 
-//retorna primeiro elemento da lista
 elo_ll* insereOrdenadoLl(elo_ll* lista, char* palavra){
     elo_ll* pos = NULL;
     if(!lista){
@@ -156,7 +161,7 @@ int comparaPalavrasOcorr(const void* a, const void* b){
 
     int n1 = (*palavra1)->ocorrencias;
     int n2 = (*palavra2)->ocorrencias;
-    int res = n1 - n2;
+    int res = n2 - n1;
     if(res == 0) res = strcmp((*palavra1)->chave, (*palavra2)->chave);
     return res;
 }
@@ -167,41 +172,237 @@ void percorreVetor(tipo_palavra** palavras, int tamanho_vetor){
     }
 }
 
+int carregaListaVetor(elo_ll* lista, tipo_palavra** palavras){
+    int tamanho_vetor = 0;
+    elo_ll* elo = lista;
+    while(elo){
+        if(tamanho_vetor >= NUM_PALAVRAS){
+            printf("Sem memória!\n");
+            exit(EXIT_FAILURE);
+        }
+        palavras[tamanho_vetor++] = elo->palavra;
+        elo_ll* ant = elo;
+        elo = elo->prox;
+        ant->prox = NULL;
+        free(ant);
+    }
+    return tamanho_vetor;
+}
+
+int buscaBinariaVetor(tipo_palavra** palavras, int tamanho_vetor, char* palavra, int* pos_insere){
+    int minimo = 0;
+    int maximo = tamanho_vetor - 1;
+    while(maximo >= minimo){
+        int alvo = (maximo + minimo)/2;
+        *pos_insere = alvo;
+        int comp = strcmp(palavras[alvo]->chave, palavra);
+        if(comp == 0) return alvo;
+        if(comp > 0)maximo = alvo - 1;
+        else{
+            minimo = alvo + 1;
+            (*pos_insere)++;
+        }
+    }
+    return -1;
+}
+
+
+int insereVetorOrd(tipo_palavra** palavras, int tamanho_vetor, char* palavra){
+    int pos_insere = 0;
+    int pos = buscaBinariaVetor(palavras, tamanho_vetor, palavra, &pos_insere);
+    if(pos >= 0) palavras[pos]->ocorrencias++;
+    else{
+        for(int i = tamanho_vetor; i > pos_insere; i--)
+            palavras[i] = palavras[i - 1];
+        adicionaVetorNaPos(palavras, pos_insere, palavra);
+        tamanho_vetor++;
+    }
+    return tamanho_vetor;
+}
+
+arvore* buscaBinariaArvore(arvore* a, char* palavra, arvore** ult){
+    while(a){
+        *ult = a;
+        int comp = strcmp(a->palavra->chave, palavra);
+        if(comp == 0) return a;
+        if(comp > 0) a = a->esq;
+        else a = a->dir;
+    }
+    return NULL;
+}
+
+arvore* insereNaArvore(arvore* a, char* palavra){
+        if(!a){
+            a = malloc(sizeof(arvore));
+            a->esq = NULL;
+            a->dir = NULL;
+            a->palavra = malloc(sizeof(tipo_palavra));
+            int tam = strlen(palavra) + 1;
+            a->palavra->chave = malloc(tam * sizeof(char));
+            strcpy(a->palavra->chave, palavra);
+            a->palavra->ocorrencias = 1;
+            return a;
+        }
+        arvore* ult = NULL;
+
+        arvore* no = buscaBinariaArvore(a, palavra, &ult);
+        if(no) no->palavra->ocorrencias++;
+        else{
+            no = malloc(sizeof(arvore));
+            no->esq = NULL;
+            no->dir = NULL;
+            no->palavra = malloc(sizeof(tipo_palavra));
+            int tam = strlen(palavra) + 1;
+            no->palavra->chave = malloc(tam * sizeof(char));
+            strcpy(no->palavra->chave, palavra);
+            no->palavra->ocorrencias = 1;
+            if(strcmp(palavra, ult->palavra->chave) > 0) ult->dir = no;
+            else ult->esq = no;
+        }
+        return a;
+}
+
+void percorreArvore(arvore* a){
+    if(!a) return;
+    percorreArvore(a->esq);
+    printf("%s %d\n", a->palavra->chave, a->palavra->ocorrencias);
+    percorreArvore(a->dir);
+}
+
+void carregaArvoreVetor(arvore* a, tipo_palavra** palavras, int* pos_vetor){
+    if(!a) return;
+    if(*pos_vetor >= NUM_PALAVRAS){
+        printf("Sem memória!\n");
+        exit(EXIT_FAILURE);
+    }
+    carregaArvoreVetor(a->esq, palavras, pos_vetor);
+    palavras[(*pos_vetor)++] = a->palavra;
+    carregaArvoreVetor(a->dir, palavras, pos_vetor);
+}
+
+void decideEstrutura(tipo_palavra** palavras, int* tamanho_vetor, elo_ll** lista, elo_ll** ultimo, \
+arvore** tree, char* estrutura, char* palavra, int* decisao){
+    if(strcmp(estrutura, "VD") == 0){
+        *tamanho_vetor = insereVetorDes(palavras, *tamanho_vetor, palavra);
+        *decisao = 1;
+    }
+    else{
+        if(strcmp(estrutura, "VO") == 0){
+            *tamanho_vetor = insereVetorOrd(palavras, *tamanho_vetor, palavra);
+            *decisao = 2;
+        }
+        else{
+            if(strcmp(estrutura, "LD") == 0){
+                *lista = insereDesordenadoLl(*lista, palavra, ultimo);
+                *decisao = 3;
+            }
+            else{
+                if(strcmp(estrutura, "LO") == 0){
+                    *lista = insereOrdenadoLl(*lista, palavra);
+                    *decisao = 4;
+                }
+                else{
+                    if(strcmp(estrutura, "AB") == 0){
+                        *tree = insereNaArvore(*tree, palavra);
+                        *decisao = 5;
+                    }
+                    else{
+                        printf("OPCAO INVALIDA!\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void mostraResultados(tipo_palavra** palavras, int tamanho_vetor, elo_ll* lista, \
+arvore* tree, int decisao, char* ordenacao){
+    int (*compare)(const void*, const void*) = NULL;
+    int o = -1;
+    if(strcmp(ordenacao, "A") == 0){
+       compare = comparaPalavrasChave;
+       o = 0;
+    }
+    else{
+        if(strcmp(ordenacao, "O") == 0){
+            compare = comparaPalavrasOcorr;
+            o = 1;
+        }
+        else{
+            printf("OPCAO INVALIDA!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    int zero = 0;
+    switch(decisao){
+        case 1:
+            qsort(palavras, tamanho_vetor, sizeof(tipo_palavra*), compare);
+            break;
+        
+        case 2:
+            if(o) qsort(palavras, tamanho_vetor, sizeof(tipo_palavra*), compare);
+            break;
+
+        case 3:
+            tamanho_vetor = carregaListaVetor(lista, palavras);
+            qsort(palavras, tamanho_vetor, sizeof(tipo_palavra*), compare);
+            break;
+        
+        case 4:
+            tamanho_vetor = carregaListaVetor(lista, palavras);
+            if(o) qsort(palavras, tamanho_vetor, sizeof(tipo_palavra*), compare);
+            break;
+
+        case 5:
+            carregaArvoreVetor(tree, palavras, &zero);
+            tamanho_vetor = zero;
+            if(o) qsort(palavras, tamanho_vetor, sizeof(tipo_palavra*), compare);
+            break;
+        
+        default:
+            printf("OPCAO INVALIDA!\n");
+            exit(EXIT_FAILURE);
+            break;
+    }
+    percorreVetor(palavras, tamanho_vetor);
+}
+
 int main(int argc, char const *argv[]){
-    if(argc < 2){
+    if(argc < 4){
         fprintf(stderr, "Número incorreto de parâmetros!\n");
         exit(EXIT_FAILURE);
     }
 
+    char* estrutura = argv[2];
+    char* ordenacao = argv[3];
+
     FILE* fp = fopen(argv[1], "r");
-    
+
     if(!fp){
         perror("Error");
         exit(EXIT_FAILURE);
     }
-
     tipo_palavra* palavras[NUM_PALAVRAS];
     int tamanho_vetor = 0; 
-
+    elo_ll* lista = NULL;
+    elo_ll* ultimo = NULL;
+    arvore* tree = NULL;
     char in[255];
+    int decisao = -1;
+
     while(fgets(in, 255, fp)){
-        char* c = in;
         const char* delim = "\" \t ( ) { } [ ] . ; , \n \' - ? ! : \\ / $ # * @";
-        char* word = strtok(c, delim);
+        char* word = strtok(in, delim);
         while(word){
             for(int i = 0; word[i] != '\0'; i++){
                 word[i] = tolower(word[i]);
             }
-            tamanho_vetor = insereVetorDes(palavras, tamanho_vetor, word);
-            if(tamanho_vetor >= NUM_PALAVRAS - 1){
-                printf("\n\n\n\n\n\n\nERRO\n\n\n\n\n\n\n\n\n");
-                printf("FALTA DE MEMORIA!\n\n\n\n");
-            }
+            decideEstrutura(palavras, &tamanho_vetor, &lista, &ultimo, &tree, estrutura, word, &decisao);
             word = strtok(NULL, delim);
         }
     }
-    qsort(palavras, tamanho_vetor, sizeof(tipo_palavra*), comparaPalavrasChave);
-    percorreVetor(palavras, tamanho_vetor);
+    mostraResultados(palavras, tamanho_vetor, lista, tree, decisao, ordenacao);
     return 0;
 }
 
